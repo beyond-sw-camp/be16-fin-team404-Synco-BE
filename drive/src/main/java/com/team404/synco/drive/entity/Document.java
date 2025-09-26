52837097d6ff73c6b7981722fe2afd5d56fa9557
kimchanjin <ihercwdxz@gmail.com>
2025-09-26 10:27:56 +0900

    feat: drive entity 추가

---
+++ drive/src/main/java/com/team404/synco/drive/entity/Document.java
@@ -0,0 +1,33 @@
+package com.team404.synco.drive.entity;
+
+import com.team404.synco.common.constant.DocumentType;
+import com.team404.synco.common.constant.YnLock;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Document extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long documentSeq;
+    private DocumentType documentType;
+    private String documentName;
+    private String documentUrl;
+    private long memberSeq;
+    @Builder.Default
+    private YnLock ynLock = YnLock.N;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT), nullable = false)
+    private Folder folder;
+    @OneToMany(mappedBy = "document")
+    private List<DocumentLine> DocumentLineList = new ArrayList<>();
+
+}
---
+++ drive/src/main/java/com/team404/synco/drive/entity/Folder.java
@@ -0,0 +1,29 @@
+package com.team404.synco.drive.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Folder extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long folderSeq;
+    private long parentFolderSeq;
+    @Column(nullable = false)
+    private String folderName;
+    @Column(nullable = false)
+    private long order;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shared_drive_channel_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT), nullable = false)
+    private DriveChannel driveChannel;
+    @OneToMany(mappedBy = "folder")
+    private List<Document> DocumentList = new ArrayList<>();
+}
