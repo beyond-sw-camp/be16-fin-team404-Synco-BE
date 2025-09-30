f01cb5f31a727ceed1e1f30261937420cc0c8499
suyeonkwong <tndusl49@naver.com>
2025-09-30 15:57:31 +0900

    refactor: task erd수정 반영

--- task/src/main/java/com/team404/synco/task/entity/Task.java
+++ task/src/main/java/com/team404/synco/task/entity/Task.java
@@ -1,5 +1,6 @@
 package com.team404.synco.task.entity;
 
+import com.team404.synco.common.constant.TaskStatus;
 import com.team404.synco.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -23,17 +24,18 @@ public class Task extends BaseEntity {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String taskContent;
     @Column(nullable = false)
-    private long picMemberSeq;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus taskStatus;
     @Column(nullable = false)
     private LocalDate startDate;
     @Column(nullable = false)
     private LocalDate endDate;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pic_member_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT), nullable = false)
+    private ScheduleManagementChannelMember picMemberSeq;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "schedule_management_channel_member_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT), nullable = false)
-    private ScheduleManagementChannelMember scheduleManagementChannelMember;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "task_status_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT), nullable = false)
-    private TaskStatus taskStatus;
+    @JoinColumn(name = "board_seq", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Board board;
     @Builder.Default
     @OneToMany(mappedBy = "task")
     private List<Comment> commentList = new ArrayList<>();
