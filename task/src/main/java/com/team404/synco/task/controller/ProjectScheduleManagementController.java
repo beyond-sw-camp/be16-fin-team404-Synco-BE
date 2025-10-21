2a1594de93fa1a39f76f4db93ddb55714f25e12c
kimchanjin <ihercwdxz@gmail.com>
2025-10-21 19:09:10 +0900

    feat: 잘못된 파일 삭제

--- task/src/main/java/com/team404/synco/task/controller/ProjectScheduleManagementController.java
+++ task/src/main/java/com/team404/synco/task/controller/ProjectScheduleManagementController.java
@@ -3,14 +3,12 @@ package com.team404.synco.task.controller;
 import com.team404.synco.common.constant.dto.ResponseDto;
 import com.team404.synco.task.dto.request.BoardCreateReqDto;
 import com.team404.synco.task.dto.request.TaskCreateReqDto;
-import com.team404.synco.task.dto.response.WorkspaceMemberDto;
 import com.team404.synco.task.service.ProjectScheduleManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,20 +19,20 @@ public class ProjectScheduleManagementController {
 
     /// 1. project space task 전체 조회
     @GetMapping("/task/{workSpaceSeq}")
-    public ResponseEntity<?> getAllTeamsTask(@PathVariable long workSpaceSeq) {
-        return ResponseEntity.status(HttpStatus.OK).body(scheduleManagementService.getAllTasksResponseDtoList(workSpaceSeq));
+    public ResponseEntity<ResponseDto<?>> getAllTeamsTask(@PathVariable long workSpaceSeq) {
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.ok(scheduleManagementService.getAllTasksResponseDtoList(workSpaceSeq), HttpStatus.OK));
     }
 
     /// 2. project space 내 board 조회 + 내가 담당자로 등록된 task 조회
     @GetMapping("/board/{workSpaceSeq}")
-    public ResponseEntity<?> getChannelBoard(@RequestHeader("X-Member-seq") long memberSeq, @PathVariable long workSpaceSeq) {
-        return ResponseEntity.status(HttpStatus.OK).body(scheduleManagementService.fetchBoardsWithTasksByChannelMember(memberSeq, workSpaceSeq));
+    public ResponseEntity<ResponseDto<?>> getChannelBoard(@RequestHeader("X-Member-seq") long memberSeq, @PathVariable long workSpaceSeq) {
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.ok(scheduleManagementService.fetchBoardsWithTasksByChannelMember(memberSeq, workSpaceSeq), HttpStatus.OK));
     }
 
     /// 4. project space 내 board 등록
     @PostMapping("/board")
-    public ResponseEntity<?> createProjectBoard(@RequestBody BoardCreateReqDto boardCreateReqDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleManagementService.createProjectBoard(boardCreateReqDto));
+    public ResponseEntity<ResponseDto<?>> createProjectBoard(@RequestBody BoardCreateReqDto boardCreateReqDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.ok(scheduleManagementService.createProjectBoard(boardCreateReqDto), HttpStatus.CREATED));
     }
 
     /// 3. project space task 등록
@@ -44,7 +42,7 @@ public class ProjectScheduleManagementController {
     }
 
     @GetMapping("/memberList/{workSpaceSeq}")
-    public ResponseEntity<?> getWorkspaceMembers(@PathVariable long workSpaceSeq) {
-        return ResponseEntity.status(HttpStatus.OK).body(scheduleManagementService.getWorkspaceMemberList(workSpaceSeq));
+    public ResponseEntity<ResponseDto<?>> getWorkspaceMembers(@PathVariable long workSpaceSeq) {
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.ok(scheduleManagementService.getWorkspaceMemberList(workSpaceSeq), HttpStatus.OK));
     }
 }
