e3ff7842f9a0f1e2efb235d92ffa931f14512e23
suyeonkwong <tndusl49@naver.com>
2025-11-03 11:10:07 +0900

    erorr

--- chat/src/main/java/com/team404/synco/common/dto/AlarmResDto.java
+++ chat/src/main/java/com/team404/synco/common/dto/AlarmResDto.java
@@ -10,18 +10,19 @@ public class AlarmResDto {
     private String alarmType;
     private String message;
     private Long workSpaceSeq;
-//    private Long targetSeq;
-    private Long channelSeq;
+    private Long targetSeq;
+//    private Long channelSeq;
 
-    public static AlarmResDto of(String receiverId, String alarmType, String message, Long workSpaceSeq, ///Long targetSeq,
-                                 Long channelSeq){
+    public static AlarmResDto of(String receiverId, String alarmType, String message, Long workSpaceSeq, Long targetSeq
+            ///, Long channelSeq
+    ){
         return AlarmResDto.builder()
                 .receiverId(receiverId)
                 .alarmType(alarmType)
                 .message(message)
                 .workSpaceSeq(workSpaceSeq)
-//                .targetSeq(targetSeq)
-                .channelSeq(channelSeq)
+                .targetSeq(targetSeq)
+//                .channelSeq(channelSeq)
                 .build();
     }
 }
